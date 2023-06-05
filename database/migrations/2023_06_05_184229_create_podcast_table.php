90f86b10f4b3d5620477b3b99e7aee71c06b8638
Miguel Ángel <miguelblangon@gmail.com>
2023-06-05 23:24:39 +0200

    Vite y boostrap

---
+++ database/migrations/2023_06_05_184229_create_podcast_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('podcast', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('detail');
+            $table->text('podcasts_url');
+            $table->text('caratula_url');
+            
+            //relación con el usuario
+            $table->unsignedBigInteger('users_id');
+            $table->foreign('users_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('podcast');
+    }
+};
